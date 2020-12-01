9de37683cb0a0f464aaf75b64b068b31132c6b7f
Dajung <Dajung@Dajung-PC>
2020-12-01 17:42:33 +0900

    final commit!

--- testForKmove/src/eea/controller/EeaController.java
+++ testForKmove/src/eea/controller/EeaController.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /**
  * Servlet implementation class EeaController
@@ -36,14 +37,17 @@ public class EeaController extends HttpServlet {
 	
 	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/html;charset=utf-8");
+		response.setContentType("application/json");
 		
 		List<EeaVO> articlesList = eeaDAO.eea(); // 요청에 대해 기사리스트를 조회
 		request.setAttribute("articlesList", articlesList); // 조회한 기사정보를 request에 바인딩
+		String gson = new Gson().toJson(articlesList);
+		response.getWriter().write(gson);
 		
-		RequestDispatcher dispatch = request.getRequestDispatcher("/f/eea.jsp"); 
-		dispatch.forward(request, response); // 컨트롤러에서 표시하고자 하는 JSP로 포워딩
-		
+		/*
+		 * RequestDispatcher dispatch = request.getRequestDispatcher("/f/eea.jsp");
+		 * dispatch.forward(request, response); // 컨트롤러에서 표시하고자 하는 JSP로 포워딩
+		 */		
 	}
 	
 		
